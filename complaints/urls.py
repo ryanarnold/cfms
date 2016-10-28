7e4677ee70783522385b94af4a2368b1452eedd0
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2016-10-28 17:32:52 +0800

    Changed the url pattern convention to use "-" instead of "_"

--- complaints/urls.py
+++ complaints/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^login/$', views.user_login, name='login'),
 	url(r'^logout/$', views.user_logout, name='logout'),
-	url(r'^new_complaint$', views.new_complaint, name='new_complaint'),
-	url(r'^search_complaint$', views.search_complaint, name='search_complaint'),
-	url(r'^update_complaint/(?P<complaint_number>[\w\-0-9]+)/$', views.update_complaint, name='update_complaint'),
+	url(r'^new-complaint$', views.new_complaint, name='new_complaint'),
+	url(r'^search-complaint$', views.search_complaint, name='search_complaint'),
+	url(r'^update-complaint/(?P<complaint_number>[\w\-0-9]+)/$', views.update_complaint, name='update_complaint'),
 ]
